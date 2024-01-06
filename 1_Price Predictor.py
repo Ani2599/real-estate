44b86526a8be61ef0237a3e1f5a2cd80354bba69
Ani2599 <116948077+Ani2599@users.noreply.github.com>
2024-01-06 23:10:43 +0530

    Add files via upload

---
+++ 1_Price Predictor.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title="Price Predictor")
+
+with open('C:\pythonProject\df.pkl','rb') as file:
+    df = pickle.load(file)
+
+with open('C:\pythonProject\pipeline.pkl','rb') as file:
+    pipeline = pickle.load(file)
+
+st.header("Enter your inputs")
+
+property_type = st.selectbox('Property Type', ['flat','house'])
+
+sector = st.selectbox('Sector', sorted(df['sector'].unique().tolist()))
+
+bedrooms = float(st.selectbox('Number of Bedrooms', sorted(df['bedRoom'].unique().tolist())))
+
+bathroom = st.selectbox('Number of Bathrooms', sorted(df['bathroom'].unique().tolist()))
+
+balcony = st.selectbox('Number of Balcony', sorted(df['balcony'].unique().tolist()))
+
+property_age = st.selectbox('Age of Property', sorted(df['agePossession'].unique().tolist()))
+
+built_up_area = float(st.number_input('Built up Area'))
+
+servant_room = float(st.selectbox('Servant Room', [0.0,1.1]))
+
+store_room = float(st.selectbox('Store Room', [0.0,1.1]))
+
+furnishing_type = st.selectbox('Furnishing Type', sorted(df['furnishing_type'].unique().tolist()))
+luxury_category = st.selectbox('Luxury Category', sorted(df['luxury_category'].unique().tolist()))
+floor_category = st.selectbox('Floor Category', sorted(df['floor_category'].unique().tolist()))
+
+if st.button('Predict'):
+    data = [[property_type,sector,bedrooms,bathroom,balcony,property_age,built_up_area,servant_room,store_room
+             ,furnishing_type,luxury_category,floor_category]]
+    columns = ['property_type', 'sector', 'bedRoom', 'bathroom', 'balcony',
+               'agePossession', 'built_up_area', 'servant room', 'store room',
+               'furnishing_type', 'luxury_category', 'floor_category']
+
+    # Convert to DataFrame
+    one_df = pd.DataFrame(data, columns=columns)
+
+    st.dataframe(one_df)
+
+    base_price = np.expm1(pipeline.predict(one_df))[0]
+    low = base_price - 0.22
+    high = base_price + 0.22
+
+    st.text("The flat of the flat is between {} Cr and {} Cr".format(low,high))
